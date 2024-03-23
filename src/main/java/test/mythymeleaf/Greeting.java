4a33c4dd539ae6edf5d74eb14024850bbddfaada
teddy <86710265+Limdongkeun@users.noreply.github.com>
2024-03-23 18:41:36 +0900

    first commit

---
+++ src/main/java/test/mythymeleaf/Greeting.java
@@ -0,0 +1,16 @@
+package test.mythymeleaf;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Greeting {
+  private long id;
+  private String content;
+
+}
